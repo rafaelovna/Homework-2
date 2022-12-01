3fa92650ce9a5ee48b53f979825bb49676bd2c96
rafaelovna <aikanush777@mail.ru>
2022-12-02 00:25:32 +0400

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -13,5 +13,25 @@ public class Main {
         System.out.println("Значение переменной seed с типом float равно "+seed);
         double weight = 7.2222222222;
         System.out.println("Значение переменной weight с типом double равно "+weight);
+
+        System.out.println("Задание 2");
+        float a = 27.12f;
+        System.out.println("Значение переменной a с типом float равно "+a);
+        long t = 987678965549L;
+        System.out.println("Значение переменной t с типом long равно "+t);
+        double g = 2;
+        System.out.println("Значение переменной g с типом double равно "+g);
+        short r = 786;
+        System.out.println("Значение переменной r с типом short равно "+r);
+        boolean f = false ;
+        System.out.println("Значение переменной f с типом boolean равно "+f);
+        char h = 569;
+        System.out.println("Значение переменной h с типом char равно "+h);
+        short b = -159;
+        System.out.println("Значение переменной b с типом short равно "+b);
+        int l = 27897;
+        System.out.println("Значение переменной l с типом int равно "+l);
+        byte n = 67;
+        System.out.println("Значение переменной n с типом byte равно "+n);
     }
 }
