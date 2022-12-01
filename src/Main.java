f7411159474ad60473fa0bb654030d147dfdef61
rafaelovna <aikanush777@mail.ru>
2022-12-01 23:37:31 +0400

    Задание 1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        int apple = 1002320008;
+        System.out.println("Значение переменной apple с типом int равно "+apple);
+        byte finance = 98;
+        System.out.println("Значение переменной finance с типом byte равно "+finance);
+        short cat = 11231;
+        System.out.println("Значение переменной cat с типом short равно "+cat);
+        long people = 325489756;
+        System.out.println("Значение переменной people с типом long равно "+people);
+        float seed = 3.1222222f;
+        System.out.println("Значение переменной seed с типом float равно "+seed);
+        double weight = 7.2222222222;
+        System.out.println("Значение переменной weight с типом double равно "+weight);
+    }
+}
